99b7623ba530e3dde2db695cc87907fa61d305fa
Ernesto Rodriguez <neto@netowork.me>
2014-08-14 00:13:56 +0200

    Implemented basic TwoFish encryption

--- src/Cryptographer/Cmd/Encrypt.hs
+++ src/Cryptographer/Cmd/Encrypt.hs
@@ -1,44 +1,93 @@
+{-# Language ScopedTypeVariables, MultiWayIf #-}
 module Cryptographer.Cmd.Encrypt where
 
 import Control.Monad (liftM)
-import Data.ByteString.Base64 (encode, decode)
-import Control.Applicative ((<$>))
+import Data.ByteString.Base64.Lazy as BE
+import Control.Applicative ((<$>), (<*>))
 import Data.ByteString as BS
-import Cryptographer.Util (hash, randIV, fromJust, tryDecode, fromEither)
-import Crypto.Cipher.Blowfish
+import qualified Codec.Encryption.Twofish as TF
+import Cryptographer.Util
 import qualified Crypto.Cipher.Types as BF
+import Data.Bits as BI
+import Data.Binary as BI
+import Data.LargeWord (Word256, Word128)
 
 type IV = ByteString
 type Key = ByteString
 
+data Cipher =
+  Blowfish256Cipher
+  | TwoFish256Cipher
+  deriving Show
+
+data Encrypted =
+  TwoFish256Enc {tfSize :: Int, tfIV :: Word128, tfData :: [Word128]}
+
+instance Binary Encrypted where
+  put (TwoFish256Enc s iv d) = do
+    put $ show TwoFish256Cipher
+    put s
+    put iv
+    put d
+  get = do
+    h <- get
+    case () of
+      _ | h == show TwoFish256Cipher ->
+        TwoFish256Enc <$> get <*> get <*> get      
+
+cbcEnc vi enc = Prelude.foldr cata []
+  where
+    cata b' [] = [enc (vi `xor` b')]
+    cata b' (b:bs) = enc (b `xor` b') : b : bs
+
+cbcDec vi dec = snd . Prelude.foldr cata (vi,[])
+  where
+    cata b' (vi,bs) = (b', dec b' `xor` vi : bs)
+
+encryptTF' k iv = cbcEnc iv $ TF.encrypt (TF.mkStdCipher k)
+
+decryptTF' k iv = cbcDec iv $ TF.decrypt (TF.mkStdCipher k)
+
+encryptTF key' text' = do
+  iv <- randIV
+  let ct = encryptTF' key iv text
+  let enc = BI.encode $ TwoFish256Enc (BS.length text') iv ct
+  return $ BE.encode enc
+  where
+    key :: Word256
+    key = Prelude.head . toBits $ hash key'
+    text = toBits text'
+  
 addPadding blockSize bs 
   | BS.length bs `div` blockSize == 0 = (bs,0)
   | otherwise = (BS.concat [bs,BS.replicate e 0], e)
   where
     e = BS.length bs `mod` blockSize
-    
-                                   
-encrypt :: Key -> ByteString -> IO (IV, ByteString)
-encrypt key' content' = do
-  (vi,bs) <- randIV
-  let
-    e = BF.cfbEncrypt cipher vi content
-  return (encode bs, encode e)
-  where
-    (content,_) = addPadding (BF.blockSize cipher) content'
-    cipher = BF.cipherInit key
-    key :: BF.Key Blowfish256
-    key = case BF.makeKey $ hash key' of
-      Right k -> k
-      Left e -> error $ "Failed to create key: " ++ show e
+
+encrypt :: Cipher -> Key -> ByteString -> IO (IV, ByteString)
+encrypt = undefined 
+        
+-- encryptGen :: forall c . BF.BlockCipher c => c -> Key -> ByteString -> IO (IV, ByteString)
+-- encryptGen _ key' content' = do
+--   (vi,bs) <- randIV
+--   let
+--     e = BF.cfbEncrypt cipher vi content
+--   return (BE.encode bs, BE.encode e)
+--   where
+--     (content,_) = addPadding (BF.blockSize cipher) content'
+--     cipher = BF.cipherInit key
+--     key :: BF.Key c
+--     key = case BF.makeKey $ hash key' of
+--       Right k -> k
+--       Left e -> error $ "Failed to create key: " ++ show e
                
 
-decrypt :: Key -> IV -> ByteString -> ByteString
-decrypt key' iv' content' = BF.cfbDecrypt cipher iv content
-  where
-    cipher = BF.cipherInit key
-    key :: BF.Key Blowfish256
-    key = fromEither $ BF.makeKey $ hash key'
-    iv = fromJust (error "Invalid initialization vector")
-         $ BF.makeIV (tryDecode iv')
-    content = tryDecode content'  
+-- decrypt :: forall c . BF.BlockCipher c => c -> Key -> IV -> ByteString -> ByteString 
+-- decrypt _ key' iv' content' = BF.cfbDecrypt cipher iv content
+--   where
+--     cipher = BF.cipherInit key
+--     key :: BF.Key c
+--     key = fromEither $ BF.makeKey $ hash key'
+--     iv = fromJust (error "Invalid initialization vector")
+--          $ BF.makeIV (tryDecode iv')
+--     content = tryDecode content'  
--- src/Cryptographer/Util.hs
+++ src/Cryptographer/Util.hs
@@ -1,3 +1,4 @@
+{-# Language ScopedTypeVariables #-}
 module Cryptographer.Util where
 
 import Data.ByteString as BS
@@ -7,6 +8,8 @@ import Data.Digest.Pure.SHA as S
 import Crypto.Cipher.Types as BF (makeIV,IV,BlockCipher)
 import Control.Applicative
 import Data.ByteString.Base64 (decode)
+import Data.Bits
+import Data.LargeWord
 
 hash :: ByteString -> ByteString
 hash = pack . BL.unpack . bytestringDigest . S.sha256 . BL.pack . unpack
@@ -26,8 +29,17 @@ fromJust a opt =
     Just o -> o
     Nothing -> a
 
-randIV :: BlockCipher c => IO (BF.IV c, ByteString)
-randIV = do
+class RandIV iv where
+  randIV :: IO iv
+
+instance BlockCipher c => RandIV (BF.IV c, ByteString) where
+  randIV = randIVBC
+
+instance RandIV Word128 where
+  randIV = return 9
+
+randIVBC :: BlockCipher c => IO (BF.IV c, ByteString)
+randIVBC = do
   (Just iv) <- return $ makeIV bs
   return (iv,bs)
   where
@@ -39,3 +51,39 @@ a >?> b = do
     Nothing -> return Nothing
     Just a'' -> b a''
 
+mkBits :: (Num b, FiniteBits b) => ByteString -> b -> b
+mkBits bs w
+  | BS.length bs == 1 = word
+  | BS.length bs > 1 = mkBits (BS.tail bs) (rotateL word (finiteBitSize byte))
+  where
+    byte = BS.head bs
+    word = w .|. fromIntegral byte
+
+toBits :: forall w . (FiniteBits w, Num w) => ByteString -> [w]
+toBits bs' = Prelude.reverse . snd $ Prelude.foldl cata (bs',[]) res'
+  where
+    s = finiteBitSize (undefined :: w)
+    b = finiteBitSize (undefined :: Word8)
+    r = s `div` b
+    l = 1 + (BS.length bs' * b) `div` s
+    res' = [1 .. l]
+    cata (bs, ws) _
+      | BS.length bs > r = (BS.drop r bs, mkBits (BS.take r bs) 0 : ws)
+      | otherwise = (bs, mkBits bs 0 : ws)
+
+mkBs :: (Integral w, FiniteBits w) => w -> ByteString -> ByteString
+mkBs wi = snd . BS.mapAccumR cata wi
+  where
+    chopper = fromIntegral $ complement (0 :: Word8)
+    cata w b = (rotateR w (finiteBitSize b), fromIntegral (w .&. chopper))
+
+fromBits :: forall w . (FiniteBits w, Integral w) => Int -> [w] -> ByteString
+fromBits _ [] = BS.empty
+fromBits l (b:bs)
+  | l > r = BS.append (mkBs b (BS.replicate r 0)) $ fromBits (l-r) bs
+  | otherwise = mkBs b (BS.replicate l 0)
+  where
+    bl = finiteBitSize (undefined :: Word8)
+    r = finiteBitSize b `div` bl
+
+-- toBits bs 
